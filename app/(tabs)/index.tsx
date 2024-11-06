fe1978e6bb267f294bc9624a2d797a40dda453c6
meanoldgit <alexantonaranda@gmail.com>
2024-11-06 17:14:28 +0100

    wheel page updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, View } from "react-native";
 import { CBackground } from '@/components/CBackground';
-import { CText } from '@/components/CText';
 import { OutlinedText } from '@/components/OutlinedText';
 import CButton from "@/components/CButton";
 import Pokeball from '@/assets/images/misc/Pokeball';
+import ButtonContainer from "@/components/ButtonContainer";
 
 
 export default function Index() {
@@ -14,11 +14,11 @@ export default function Index() {
         <OutlinedText size={75}>23</OutlinedText>
         <Pokeball width={200} height={200} />
       </View>
-      <View style={styles.buttonContainer}>
-        <CButton href={"/wheel"} />
-        <CButton href={"/team"} />
-        <CButton href={"/missions"} />
-      </View>
+      <ButtonContainer>
+        <CButton type="link" href="/wheel" />
+        <CButton type="link" href="/team" />
+        <CButton type="link" href="/missions" />
+      </ButtonContainer>
     </CBackground>
   );
 }
@@ -28,16 +28,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
-  },
-  buttonContainer: {
-    width:"100%",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    padding: 10,
-    borderRadius: 15,
-    gap: 10,
-    // backgroundColor: "#ffffff33",
-    backgroundColor: "#0C0C0D33"
   }
 });
--- app/(tabs)/wheel.tsx
+++ app/(tabs)/wheel.tsx
@@ -1,7 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import { Image } from 'expo-image';
 import { CBackground } from '@/components/CBackground';
 import { CText } from '@/components/CText';
+import { View } from 'react-native';
+import ButtonContainer from '@/components/ButtonContainer';
+import CButton from '@/components/CButton';
 
 
 const WheelImage = require("@/assets/images/misc/wheel.png");
@@ -10,18 +13,40 @@ const WheelImage = require("@/assets/images/misc/wheel.png");
 export default function Wheel() {
   return (
     <CBackground>
-      <Image source={WheelImage} style={styles.wheel} />
-      <CText>Wheel</CText>
+      <View style={styles.container}>
+        <Image source={WheelImage} style={styles.wheel} />
+      </View>
+      <View style={styles.tries}>
+        <CText size={30}>x 10</CText>
+      </View>
+      <ButtonContainer>
+        <CButton type="ad" />
+        <CButton type="spin" />
+      </ButtonContainer>
     </CBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    color: '#fff',
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
   wheel: {
     width: "85%",
     aspectRatio: 1
+  },
+  tries: {
+    marginBottom: 20,
+    width: "35%",
+    alignSelf: "flex-end",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    borderRadius: 15,
+    gap: 10,
+    // backgroundColor: "#ffffff33",
+    backgroundColor: "#0C0C0D33"
   }
 });
