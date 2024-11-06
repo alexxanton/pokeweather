e4887a46167baf62cb887a14723698a72d3d5fa6
meanoldgit <alexantonaranda@gmail.com>
2024-11-06 17:13:16 +0100

    CText updated to take size prop

--- components/CText.tsx
+++ components/CText.tsx
@@ -1,9 +1,9 @@
 import { PropsWithChildren } from "react";
 import { Text, StyleSheet } from "react-native";
 
-export function CText({ children }: PropsWithChildren) {
+export function CText({ children, size }: PropsWithChildren & {size:number}) {
   return (
-    <Text style={styles.text}>
+    <Text style={[styles.text, {fontSize:size}]}>
       {children}
     </Text>
   );
