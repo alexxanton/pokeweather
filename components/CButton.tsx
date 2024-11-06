8023ab9fdb74e068e1d35b930deed94122cc7322
meanoldgit <alexantonaranda@gmail.com>
2024-11-06 17:14:12 +0100

    added CButton press handlers

--- components/CButton.tsx
+++ components/CButton.tsx
@@ -1,22 +1,52 @@
-import { Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Vibration } from "react-native";
 import { useRouter } from "expo-router";
 import WheelButton from '@/assets/images/buttons/WheelButton';
+import { useState } from "react";
 
 type Props = {
-  href: string
+  type: string,
+  href?: string
 }
 
-export default function CButton({href}: Props) {
+export default function CButton({type, href}: Props) {
   const router = useRouter();
+  const [buttonPressed, setButtonPressed] = useState(false);
+  const [scale, setScale] = useState(1);
+
+  const styles = StyleSheet.create({
+    btn: {
+      transform: [{scale}]
+    },
+  });
+
+  const handlePress = () => {
+    if (!buttonPressed) {
+      router.push(href);
+    }
+    
+    setButtonPressed(true);
+    setTimeout(() => {
+      setButtonPressed(false);
+    }, 500);
+  }
+
+  const handlePressIn = () => {
+    Vibration.vibrate(100);
+    setScale(1.1);
+  };
+
+  const handlePressOut = () => {
+    setScale(1);
+  }
 
   return (
-    <Pressable onPress={() => router.push(href)} style={styles.btn}>
+    <Pressable
+      onPress={handlePress}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      style={styles.btn}
+    >
       <WheelButton width={90} height={90} />
     </Pressable>
   );
 }
-
-const styles = StyleSheet.create({
-  btn: {
-  }
-});
