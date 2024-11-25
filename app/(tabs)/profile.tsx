a39d85ac55e7a94528ba22af6ba6490e3ae2ec4d
meanoldgit <alexantonaranda@gmail.com>
2024-11-25 19:07:41 +0100

    db connected with app

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,33 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View } from 'react-native';
 import { CBackground } from '@/components/CBackground';
-import { CContainer } from "@/components/CContainer";
-import { CButton } from "@/components/CButton";
-import { BackButton } from "@/components/navigation/BackButton";
+import { CContainer } from '@/components/CContainer';
+import { CButton } from '@/components/CButton';
+import { BackButton } from '@/components/navigation/BackButton';
+import axios from 'axios';
 
 import SignoutButton from '@/assets/images/buttons/SignoutButton';
+import { useEffect, useState } from 'react';
+import { CText } from '@/components/CText';
 
 export default function Team() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const response = await axios.get("https://pokeweather.loca.lt/user");
+    setUsers(response.data);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <CBackground>
       <BackButton />
       <View style={styles.container}>
+        {users ? users.map((user, idx) => {
+          return <CText size={45} key={idx}>{user.mail}</CText>
+        }) : <CText size={45}>e</CText>}
       </View>
       <CContainer>
         <CButton>
--- app/(tabs)/team.tsx
+++ app/(tabs)/team.tsx
@@ -2,20 +2,44 @@ import { StyleSheet, View } from "react-native";
 import { CBackground } from '@/components/CBackground';
 import { CContainer } from "@/components/CContainer";
 import { CButton } from "@/components/CButton";
+import { BackButton } from "@/components/navigation/BackButton";
+import { Image } from 'expo-image';
+import axios from "axios";
 
 import BoostButton from '@/assets/images/buttons/BoostButton';
 import BattleButton from '@/assets/images/buttons/BattleButton';
-import { BackButton } from "@/components/navigation/BackButton";
+import { useEffect, useState } from "react";
+import { CText } from "@/components/CText";
+import { CPokemonButton } from "@/components/CPokemonButton";
 
 export default function Team() {
+  const [pokemon, setPokemon] = useState([]);
+  
+  const fetchPokemon = async () => {
+    const response = await axios.get("https://pokeweather.loca.lt/pokemon");
+    setPokemon(response.data);
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
   return (
     <CBackground>
-
       <BackButton />
       <View style={styles.container}>
-        
+        <CContainer>
+          {pokemon.length > 0 ? (
+            pokemon.map((pkmn, key) => {
+              return (
+                <CPokemonButton id={pkmn.id} key={key} />
+              )
+            })
+          ) : (
+            <CText size={50}>Loading...</CText>
+          )}
+        </CContainer>
       </View>
-
       <CContainer>
         <CButton href="/boost">
           <BoostButton width={135} height={90} />
@@ -24,13 +48,12 @@ export default function Team() {
           <BattleButton width={135} height={90} />
         </CButton>
       </CContainer>
-
     </CBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
